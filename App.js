22585298b7ed8a166bf7a7d531f88397db484630
tdewangan <tribhuwan.dewangan@gmail.com>
2017-12-02 23:48:22 +0530

    navigation implimentation with Main screen

--- App.js
+++ App.js
@@ -1,28 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 import store from './store';
+import MainScreen from './screens/MainScreen';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
+    const MainNavigator = StackNavigator({
+      Main: { screen: MainScreen }
+    });
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-        </View>
-      </Provider> 
+        <MainNavigator />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/MainScreen.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Platform } from 'react-native';
+import { STATUS_BAR_HEIGHT } from './../constants';
+
+class MainScreen extends Component {
+    static navigationOptions = () => ({
+        title: 'Capo Keys',
+        headerStyle: {
+            height: Platform.OS === 'android' ? 54 + STATUS_BAR_HEIGHT : 54,
+            backgroundColor: '#2196F3'
+        },
+        headerTitleStyle: {
+            marginTop: Platform.OS === 'android' ? STATUS_BAR_HEIGHT : 0,
+            color: 'white'
+        },
+        headerLeft: <View>I</View>
+    });
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: '#ddd' }}>
+                {/* Modal */}
+
+                {/* Content */}
+            </View>
+        );
+    }
+}
+
+export default MainScreen;
