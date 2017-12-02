e5ff55e27eeae86fbb9a552ee9ec83cb0c34d21a
tdewangan <tribhuwan.dewangan@gmail.com>
2017-12-03 00:29:01 +0530

    added logo, added into splash and main screen, implemented cache for images

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-import { View, Platform, Text } from 'react-native';
+import { View, Platform, Image } from 'react-native';
+import Expo from 'expo';
+import icon from './../assets/icons/app-icon.png';
 import { STATUS_BAR_HEIGHT } from './../constants';
+
+const cacheImages = (images) => images.map(image => {
+    if (typeof image === 'string') return Image.prefetch(image);
+    return Expo.Asset.fromModule(image).downloadAsync();
+});
 
 class MainScreen extends Component {
     static navigationOptions = () => ({
@@ -13,8 +20,24 @@ class MainScreen extends Component {
             marginTop: Platform.OS === 'android' ? STATUS_BAR_HEIGHT : 0,
             color: 'white'
         },
-        headerLeft: <View><Text>I</Text></View>
+        headerLeft: (
+            <Image
+                source={icon}
+                style={styles.logoStyle}
+            />
+        )
     });
+    state = {
+        appIsReady: false
+    }
+    componentWillMount() {
+        this.loadAssetsAsync();
+    }
+    async loadAssetsAsync() {
+        const imageAssets = cacheImages([icon]);
+        await Promise.all([...imageAssets]);
+        this.setState({ appIsReady: true });
+    }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#ddd' }}>
@@ -24,6 +47,14 @@ class MainScreen extends Component {
             </View>
         );
     }
-}
 
+}
+const styles = {
+    logoStyle: {
+        marginTop: 20,
+        marginLeft: 10,
+        width: 40,
+        height: 40
+    }
+};
 export default MainScreen;
